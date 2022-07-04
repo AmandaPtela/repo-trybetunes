a3074c88776a901354e9199dd07c6dd71ff42a23
Amanda Portela <mandaptela@gmail.com>
2022-07-04 16:37:41 -0300

    Requisito 5 - Input pesquisa

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,15 +3,42 @@ import PropTypes from 'prop-types';
 import Header from './Header';
 
 class Search extends React.Component {
+  state = {
+    artistaBuscado: '',
+    buttonDisabled: true,
+  }
+
+  handleInput = (event) => {
+    const { artistaBuscado } = this.state;
+    this.setState({ artistaBuscado: event.target.value });
+    const tamanho = artistaBuscado.length;
+    const min = 1;
+    if (tamanho >= min) {
+      this.setState({ buttonDisabled: false });
+    }
+    if (tamanho < min) {
+      this.setState({ buttonDisabled: true });
+    }
+  }
+
   render() {
     const { nomeUsuario } = this.props;
+    const { buttonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header nomeUserr={ nomeUsuario } />
-        <input type="text" />
-        <button type="submit">Buscar</button>
-        <p>Resultado PESQUISA</p>
-        {/* rota: /search */}
+        <input
+          data-testid="search-artist-input"
+          type="text"
+          onChange={ this.handleInput }
+        />
+        <button
+          data-testid="search-artist-button"
+          type="submit"
+          disabled={ buttonDisabled }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
